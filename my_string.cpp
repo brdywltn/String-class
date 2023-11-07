84ec06cd6dce0ca57c715287b1256af97277f401
Brody <brody2.wilton@live.uwe.ac.uk>
2023-11-07 16:44:03 +0000

    Implement RC class

--- my_string.cpp
+++ my_string.cpp
@@ -1,35 +1,40 @@
 #include "my_string.hpp"
 
-my_string::my_string() {
+template<>
+my_string<int>::my_string() {
     full_string = nullptr;
 }
 
-my_string::my_string(const char* str) {
+template<>
+my_string<int>::my_string(const char* str) {
     full_string = new char[strlen(str) + 1];
     strcpy(full_string, str);
     refCount = new int(1);
 }
 
-
-my_string::my_string(my_string const& s) {
+template<>
+my_string<int>::my_string(my_string const& s) {
     full_string = new char[strlen(s.full_string) + 1];
     strcpy(full_string, s.full_string);
     refCount = s.refCount;
     *refCount = *refCount + 1;
 }
 
-my_string& my_string::operator=(my_string const& s) {
+template<>
+my_string<int>& my_string<int>::operator=(my_string const& s) {
     delete[] full_string;
     full_string = new char[strlen(s.full_string)];
     full_string = s.full_string;
 }
 
-my_string::~my_string() {
+template<>
+my_string<int>::~my_string() {
     *refCount = *refCount - 1;
     if ((*refCount - 1) == 0) delete[] full_string;
 } 
 
-char my_string::getChar(const int& i) const {
+template<>
+char my_string<int>::getChar(const int& i) const {
     for (int j = 0; j < strlen(full_string); ++j){
         if (i == j) {
             return full_string[j];
@@ -37,7 +42,8 @@ char my_string::getChar(const int& i) const {
     }
 }
 
-void my_string::setChar(const int& i, const char& c) {
+template<>
+void my_string<int>::setChar(const int& i, const char& c) {
    char ch = i;
 
    for (int j = 0; j < strlen(full_string); ++j) {
@@ -47,7 +53,8 @@ void my_string::setChar(const int& i, const char& c) {
    }
 }
 
-void my_string::print() const {
+template<>
+void my_string<int>::print() const {
     char * string_to_print = full_string;
     
     for (int i = 0; i < strlen(string_to_print); ++i){
